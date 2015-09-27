5a573c5b62fbdfc4bc8ff1242d0cc2197db25a98
Ashwini <ashwini1484@hotmail.com>
2015-09-27 23:23:10 +0530

    Cache the matrix and retrieve if same operation is performed

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,36 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
+makeCacheMatrix <- function(setmatrix = matrix()) 
+                   {
+                      Inverse <- NULL
+                      setMatrix  <- function(x)
+                      {
+                          setmatrix <<- x
+                          Inverse <<- NULL
+                      }
+                      getMatrix <- function() setmatrix
+                      setInverse <- function(x) Inverse <<- x
+                      getInverse <- function() Inverse
+                      list(setMatrix = setMatrix,getMatrix=getMatrix,
+                           setInverse=setInverse,getInverse=getInverse)
+                   }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) 
+              {
         ## Return a matrix that is the inverse of 'x'
-}
+                 Inverse <- x$getInverse()
+                 if(!is.null(Inverse)) 
+                 {
+                    message("getting cached data")
+                    return(Inverse)
+                 }
+                 data <- x$getMatrix()
+                 Inverse <- solve(data)
+                 x$setInverse(Inverse)
+                 Inverse
+              }
+
